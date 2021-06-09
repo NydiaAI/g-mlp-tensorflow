558a79e63d98b4b894fe08ed48abb05394f93d0c
Alex Pilafian <sikanrong@gmail.com>
2021-06-09 21:36:30 +0100

    Update package name to reflect repository name (dash after the g)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='gmlp-tensorflow',
+setup(name='g-mlp-tensorflow',
       version='0.0.1',
       description='A gMLP (gated MLP) implementation in Tensorflow 1.x, as per the paper Pay Attention to MLPs 2105.08050',
       author='Nydia AI, LLC',
