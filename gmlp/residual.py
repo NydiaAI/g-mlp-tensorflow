823c84c1f143639c1887b574fb0deb66817943c8
trblair82 <trblair82@gmail.com>
2021-06-14 15:23:43 +0100

    Updating residual to be rezero.

--- gmlp/residual.py
+++ gmlp/residual.py
@@ -5,6 +5,9 @@ class Residual(Layer):
     def __init__(self, fn, **kwargs):
         super().__init__(**kwargs)
         self.fn = fn
-        
+
+    def build(self, shape):
+        init_gates = tf.ones((shape[1], shape[2]), dtype=tf.float32)
+        self.alpha = tf.Variable(init_gates, name='rezero_alpha')
     def call(self, x):
-        return self.fn(x) + x
+        return self.alpha * self.fn(x) + x
