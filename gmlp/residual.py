4ca0d0e8b38cccf74f5ccab83d23a82e2e1fd741
trblair82 <trblair82@gmail.com>
2021-06-14 15:26:04 +0100

    Cleaning up Residual module

--- gmlp/residual.py
+++ gmlp/residual.py
@@ -7,7 +7,7 @@ class Residual(Layer):
         self.fn = fn
 
     def build(self, shape):
-        init_gates = tf.ones((shape[1], shape[2]), dtype=tf.float32)
+        init_gates = tf.ones((shape[1:]), dtype=tf.float32)
         self.alpha = tf.Variable(init_gates, name='rezero_alpha')
     def call(self, x):
         return self.alpha * self.fn(x) + x
